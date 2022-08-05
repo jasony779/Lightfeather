fbbda6c8c1da9f0701ffa375a6874d713dcbcdde
Jason Yoon <jasony779@gmail.com>
2022-08-05 13:52:33 -0700

    after mui

---
+++ src/layout/content.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
+
+const MainCard = () => {
+  return (
+    <Card></>
+  ) 
+}
+
